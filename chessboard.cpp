0282a007b4b17cf75431e96393a4228b3836372e
Haruki Yoshida <yoshidaharuki@Harukis-MacBook-Pro.local>
2023-12-22 21:40:45 -0600

    Initial commit of files

---
+++ chessboard.cpp
@@ -0,0 +1,1 @@
+#include "src/chessboard.h"
---
+++ src/chessboard.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+const int BOARD_SIZE = 8;
+const int WHITE_PLAYER = 1;
+const int BLACK_PLAYER = -1;
+
+class ChessSquare
+{
+public:
+    int row;
+    int col;
+}
+
+class ChessBoard
+{
+public:
+}
